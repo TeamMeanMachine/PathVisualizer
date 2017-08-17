a6f8ec0088f888505ecfdd2fd6b542015cf1c143
Jeb Bush <picklestirfry@hotmail.com>
2017-08-16 19:59:13 -0700

    Point class

---
+++ src/main/java/org/team2471/frc/pathvisualizer/Point.java
@@ -0,0 +1,20 @@
+package org.team2471.frc.pathvisualizer;
+
+/**
+ * Created by pickl on 8/16/2017.
+ */
+public class Point {
+    private int x;
+    private int y;
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+
+}
