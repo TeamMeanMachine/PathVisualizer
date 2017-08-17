1620bc899f29f7bbb42c8585b416df874a97251d
Jeb Bush <picklestirfry@hotmail.com>
2017-08-16 20:16:32 -0700

    frame creation in main

--- src/main/java/org/team2471/frc/pathvisualizer/Main.java
+++ src/main/java/org/team2471/frc/pathvisualizer/Main.java
@@ -2,5 +2,8 @@ package org.team2471.frc.pathvisualizer;
 
 public class Main {
   public static void main(String[] args) {
+      PathVisualizer pvFrame = new PathVisualizer();
+      pvFrame.setSize(600, 600);
+      pvFrame.setVisible(true);
   }
 }
--- src/main/java/org/team2471/frc/pathvisualizer/PathVisualizer.java
+++ src/main/java/org/team2471/frc/pathvisualizer/PathVisualizer.java
@@ -1,6 +1,11 @@
 package org.team2471.frc.pathvisualizer;
 
 import javax.swing.*;
+import javax.swing.event.MouseInputAdapter;
 
 public class PathVisualizer extends JFrame {
+
+    private class onClickHandler extends MouseInputAdapter{
+        
+    }
 }
