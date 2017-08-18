281396585955bfbf4dcdc7ccae0e2dda263fbeb9
Jeb Bush <picklestirfry@hotmail.com>
2017-08-17 18:49:52 -0700

    added event listener to combobox

--- src/main/java/org/team2471/frc/pathvisualizer/PathVisualizer.java
+++ src/main/java/org/team2471/frc/pathvisualizer/PathVisualizer.java
@@ -4,6 +4,8 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.MouseInputAdapter;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,11 +15,26 @@ public class PathVisualizer extends JFrame {
     private BufferedImage blueSideImage;
     private BufferedImage redSideImage;
     private JComboBox sideSelection;
-    private boolean blueTrueRedFalse;
+    private enum Sides{BLUE, RED}
+    Sides sides;
     PathVisualizer(){
-        blueTrueRedFalse = true;
+        sides = Sides.BLUE;
         String[] comboBoxNames = {"Blue Side", "Red Side"};
         sideSelection = new JComboBox(comboBoxNames);
+        sideSelection.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                if(e.getStateChange() == ItemEvent.SELECTED){
+                    if (sideSelection.getSelectedIndex() == 0){
+                        sides = Sides.BLUE;
+                    }
+                    else if(sideSelection.getSelectedIndex() == 1){
+                        sides = Sides.RED;
+                    }
+                }
+            }
+        });
+        add(sideSelection);
+
         try{
             blueSideImage = ImageIO.read(new File("HalfFieldDiagramBlue.png"));
         } catch (IOException e){}
@@ -27,8 +44,8 @@ public class PathVisualizer extends JFrame {
 
     }
     public void paintComponent(Graphics g){
-        if(blueTrueRedFalse) g.drawImage(blueSideImage, 0, 0, null);
-        else g.drawImage(redSideImage, 0, 0, null);
+        if(sides == Sides.BLUE) g.drawImage(blueSideImage, 0, 0, null);
+        else if(sides == Sides.RED) g.drawImage(redSideImage, 0, 0, null);
     }
     private class onClickHandler extends MouseInputAdapter{
 
