b3d1578783d12cfd4ac9b68b1e80a380db9a7e38
tylerscheuble <tylerscheuble@gmail.com>
2017-08-22 20:21:24 -0700

    semicolons

--- src/main/kotlin/org/team2471/frc/pathvisualizer/DefaultPath.kt
+++ src/main/kotlin/org/team2471/frc/pathvisualizer/DefaultPath.kt
@@ -2,19 +2,18 @@ package org.team2471.frc.pathvisualizer
 
 import org.team2471.frc.lib.motion_profiling.Path2D
 
-
 object DefaultPath : Path2D() {
   init {
-    addPointAndTangent(0.0, 0.0, 0.0, 6.0);
-    addPointAndTangent(-4.2, 7.0, -6.0, 3.0);
+    addPointAndTangent(0.0, 0.0, 0.0, 6.0)
+    addPointAndTangent(-4.2, 7.0, -6.0, 3.0)
 
-    addPointAndTangent(-4.2, 7.0, 6.0, -3.0);
-    addPointAndTangent(-0.0, 0.0, -0.0, -6.0);
+    addPointAndTangent(-4.2, 7.0, 6.0, -3.0)
+    addPointAndTangent(-0.0, 0.0, -0.0, -6.0)
 
-    addPointAndTangent(-0.0, 0.0, 0.0, 6.0);
-    addPointAndTangent(7.0, 16.0, 8.0, 0.0);
+    addPointAndTangent(-0.0, 0.0, 0.0, 6.0)
+    addPointAndTangent(7.0, 16.0, 8.0, 0.0)
 
-    addEasePoint(0.0, 0.0);
-    addEasePoint(3.0, 1.0);
+    addEasePoint(0.0, 0.0)
+    addEasePoint(3.0, 1.0)
   }
 }
